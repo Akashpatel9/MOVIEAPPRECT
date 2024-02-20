db26e0184c70382b6b6c10d2a3c7facb3e18f04a
akash <akashpatelsingh9893693091.com>
2024-02-20 17:31:59 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Router from './assets/Router'
 
 const App = () => {
   return (
-    <div className="bg-zinc-900 h-screen w-full">
+    <div className="bg-zinc-900 h-screen w-full overflow-x-hidden">
       <Router/>
     </div>
   );
